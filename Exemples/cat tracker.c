b240a1f1c1754525bfafb7754cc2b91c2faa4797
sebcah <sebastien.cah@gmail.com>
2015-12-18 11:54:25 +0100

    le chat a un GPS

---
+++ Exemples/cat tracker.c
@@ -0,0 +1,333 @@
+//#include <b64.h>
+#include <HttpClient.h>
+#include <LTask.h>
+#include <LWiFi.h>
+#include <LWiFiClient.h>
+#include <LDateTime.h>
+#define WIFI_AP "Wifi Network Name"
+#define WIFI_PASSWORD "wifi password"
+#define WIFI_AUTH LWIFI_WPA  // choose from LWIFI_OPEN, LWIFI_WPA, or LWIFI_WEP.
+#define per 50
+#define per1 3
+#define DEVICEID "device ID from mediatek" // Input your deviceId
+#define DEVICEKEY "device key from mediatek" // Input your deviceKey
+#define SITE_URL "api.mediatek.com"
+#include <LGPS.h>
+double latitude, longitude;
+int tmp;
+char* x;
+gpsSentenceInfoStruct info;
+char buff[256];
+
+LWiFiClient c;
+unsigned int rtc;
+unsigned int lrtc;
+unsigned int rtc1;
+unsigned int lrtc1;
+char port[4]={0};
+char connection_info[21]={0};
+char ip[21]={0};             
+int portnum;
+int val = 0;
+String tcpdata = String(DEVICEID) + "," + String(DEVICEKEY) + ",0";
+String upload_led;
+String tcpcmd_led_on = "LED_Control,1";
+String tcpcmd_led_off = "LED_Control,0";
+
+LWiFiClient c2;
+HttpClient http(c2);
+
+void setup()
+{
+  
+  LTask.begin();
+  LWiFi.begin();
+  Serial.begin(115200);
+  LGPS.powerOn();
+  while(!Serial) delay(1000); /* comment out this line when Serial is not present, ie. run this demo without connect to PC */
+
+  Serial.println("Connecting to AP");
+  while (0 == LWiFi.connect(WIFI_AP, LWiFiLoginInfo(WIFI_AUTH, WIFI_PASSWORD)))
+  {
+    delay(1000);
+  }
+  
+  Serial.println("calling connection");
+
+  while (!c2.connect(SITE_URL, 80))
+  {
+    Serial.println("Re-Connecting to WebSite");
+    delay(1000);
+  }
+  delay(100);
+
+  pinMode(13, OUTPUT);
+  getconnectInfo();
+  connectTCP();
+}
+
+void getconnectInfo(){
+  //calling RESTful API to get TCP socket connection
+  c2.print("GET /mcs/v2/devices/");
+  c2.print(DEVICEID);
+  c2.println("/connections.csv HTTP/1.1");
+  c2.print("Host: ");
+  c2.println(SITE_URL);
+  c2.print("deviceKey: ");
+  c2.println(DEVICEKEY);
+  c2.println("Connection: close");
+  c2.println();
+  
+  delay(500);
+
+  int errorcount = 0;
+  while (!c2.available())
+  {
+    Serial.println("waiting HTTP response: ");
+    Serial.println(errorcount);
+    errorcount += 1;
+    if (errorcount > 10) {
+      c2.stop();
+      return;
+    }
+    delay(100);
+  }
+  int err = http.skipResponseHeaders();
+
+  int bodyLen = http.contentLength();
+  Serial.print("Content length is: ");
+  Serial.println(bodyLen);
+  Serial.println();
+  char c;
+  int ipcount = 0;
+  int count = 0;
+  int separater = 0;
+  while (c2)
+  {
+    int v = c2.read();
+    if (v != -1)
+    {
+      c = v;
+      Serial.print(c);
+      connection_info[ipcount]=c;
+      if(c==',')
+      separater=ipcount;
+      ipcount++;    
+    }
+    else
+    {
+      Serial.println("no more content, disconnect");
+      c2.stop();
+
+    }
+    
+  }
+  Serial.print("The connection info: ");
+  Serial.println(connection_info);
+  int i;
+  for(i=0;i<separater;i++)
+  {  ip[i]=connection_info[i];
+  }
+  int j=0;
+  separater++;
+  for(i=separater;i<21 && j<5;i++)
+  {  port[j]=connection_info[i];
+     j++;
+  }
+  Serial.println("The TCP Socket connection instructions:");
+  Serial.print("IP: ");
+  Serial.println(ip);
+  Serial.print("Port: ");
+  Serial.println(port);
+  portnum = atoi (port);
+  Serial.println(portnum);
+
+} //getconnectInfo
+
+void uploadstatus(){
+
+  Serial.println("LGPS loop"); 
+  LGPS.getData(&info);
+  Serial.println((char*)info.GPGGA); 
+  tmp = getComma(2, (char*)info.GPGGA);
+  x = (char*)info.GPGGA;
+  latitude = getDoubleNumber(&x[tmp]);
+  tmp = getComma(4, x);
+  longitude = getDoubleNumber(&x[tmp]);
+  Serial.println(latitude/100.0,6);
+  Serial.println(longitude);
+  
+  //calling RESTful API to upload datapoint to MCS to report LED status
+  Serial.println("calling connection");
+  LWiFiClient c2;  
+
+  while (!c2.connect(SITE_URL, 80))
+  {
+    Serial.println("Re-Connecting to WebSite");
+    delay(1000);
+  }
+
+  // The GPS location is based on the latitude, longitude, and meters above sea level
+    
+    //upload_led = 'GPS_Location,,' + longitude + ','  + latitude + ',806';
+
+    upload_led = "GPS_Location,,";
+    upload_led = upload_led + String(longitude/100.0,6);
+    upload_led = upload_led + ",-";
+    upload_led = upload_led + String(latitude/100.0,6);
+    tmp = getComma(9, x);
+    upload_led = upload_led + ",";
+    upload_led = upload_led + getDoubleNumber(&x[tmp]);
+    
+  Serial.println(upload_led);
+  
+  int thislength = upload_led.length();
+  
+  HttpClient http(c2);
+  c2.print("POST /mcs/v2/devices/");
+  c2.print(DEVICEID);
+  c2.println("/datapoints.csv HTTP/1.1");
+  c2.print("Host: ");
+  c2.println(SITE_URL);
+  c2.print("deviceKey: ");
+  c2.println(DEVICEKEY);
+  c2.print("Content-Length: ");
+  c2.println(thislength);
+  c2.println("Content-Type: text/csv");
+  c2.println("Connection: close");
+  c2.println(); 
+  c2.println(upload_led);
+
+  
+  
+  delay(500);
+
+  int errorcount = 0;
+  while (!c2.available())
+  {
+    Serial.print("waiting HTTP response: ");
+    Serial.println(errorcount);
+    errorcount += 1;
+    if (errorcount > 10) {
+      c2.stop();
+      return;
+    }
+    delay(100);
+  }
+  int err = http.skipResponseHeaders();
+
+  int bodyLen = http.contentLength();
+  Serial.print("Content length is: ");
+  Serial.println(bodyLen);
+  Serial.println();
+  while (c2)
+  {
+    int v = c2.read();
+    if (v != -1)
+    {
+      Serial.print(char(v));
+    }
+    else
+    {
+      Serial.println("no more content, disconnect");
+      c2.stop();
+
+    }
+    
+  }
+}
+
+
+
+void connectTCP(){
+  //establish TCP connection with TCP Server with designate IP and Port
+  c.stop();
+  Serial.println("Connecting to TCP");
+  Serial.println(ip);
+  Serial.println(portnum);
+  while (0 == c.connect(ip, portnum))
+  {
+    Serial.println("Re-Connecting to TCP");    
+    delay(1000);
+  }  
+  Serial.println("send TCP connect");
+  c.println(tcpdata);
+  c.println();
+  Serial.println("waiting TCP response:");
+} //connectTCP
+
+void heartBeat(){
+  Serial.println("send TCP heartBeat");
+  c.println(tcpdata);
+  c.println();
+    
+} //heartBeat
+
+void loop()
+{
+  //Check for TCP socket command from MCS Server 
+  String tcpcmd="";
+  while (c.available())
+   {
+      int v = c.read();
+      if (v != -1)
+      {
+        Serial.print((char)v);
+        tcpcmd += (char)v;
+
+        // substring(40) looks at the 40th position in the string (which so happens to be when it starts with LED_Control)
+        // If this is equal to LED_Control,1 then I should turn it on, if it's LED_Control,0 then turn it off
+        if (tcpcmd.substring(40).equals(tcpcmd_led_on)){
+          digitalWrite(13, HIGH);
+          Serial.println("Switch LED ON ");
+          tcpcmd="";
+        }else if(tcpcmd.substring(40).equals(tcpcmd_led_off)){  
+          digitalWrite(13, LOW);
+          Serial.println("Switch LED OFF");
+          tcpcmd="";
+        }
+      }
+   }
+
+  LDateTime.getRtc(&rtc);
+  if ((rtc - lrtc) >= per) {
+    heartBeat();
+    lrtc = rtc;
+  }
+  //Check for report datapoint status interval
+  LDateTime.getRtc(&rtc1);
+  if ((rtc1 - lrtc1) >= per1) {
+    uploadstatus();
+    lrtc1 = rtc1;
+  }
+  
+}
+
+static double getDoubleNumber(const char *s)
+{
+  char buf[10];
+  unsigned char i;
+  double rev;
+  
+  i=getComma(1, s);
+  i = i - 1;
+  strncpy(buf, s, i);
+  buf[i] = 0;
+  rev=atof(buf);
+  Serial.println(buf);
+  return rev; 
+}
+
+static unsigned char getComma(unsigned char num,const char *str)
+{
+  unsigned char i,j = 0;
+  int len=strlen(str);
+  for(i = 0;i < len;i ++)
+  {
+     if(str[i] == ',')
+      j++;
+     if(j == num)
+      return i + 1; 
+  }
+  return 0; 
+}
