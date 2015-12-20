c323470180b98ccf7ac457285150cb16ae6d60f1
sebcah <sebastien.cah@gmail.com>
2015-12-20 16:44:04 +0100

    Envoie de la position GPS par SMS
    Fonctionne, ready pour Master
    Fonction Wifi commentée

---
+++ GPS_WIFI_LED_GSM_Seb/GPS_WIFI_LED_GSM_Seb.ino
@@ -0,0 +1,63 @@
+//Connection GPS
+#include <LGPS.h> //C:\Program Files (x86)\LinkIt-ONE-IDE-master\hardware\arduino\mtk\libraries\LGPS
+#include <LGSM.h>
+#include <HttpClient.h> //téléchargé et ajouté à la lib depuis le net https://github.com/amcewen/HttpClient/releases
+#include <LWiFi.h>
+#include <LDateTime.h>
+
+#include "position.h"
+#include "wificonnect.h"
+#include "connectgsm.h"
+
+//Vérifier + Téléverser le code sur le PORT6 Debug, ouvrir le terminal sur le PORT3 Modem pour visualiser
+gpsSentenceInfoStruct info;
+
+void setup() {
+  // put your setup code here, to run once:obligatoire, même vide
+  
+  pinMode(13, OUTPUT);
+  Serial.begin(115200);
+  
+  /******connection internet******/
+  /*LTask.begin();
+  LWiFi.begin();
+  wifiprint(); // affichage console etat TCP-IP
+  getconnectInfo();
+  connectTCP();*/
+  /******connection GPS******/
+  LGPS.powerOn();
+  Serial.println("Demarrage GPS..."); 
+  /*****connection GSM*******/
+      while(!LSMS.ready())
+    {
+        delay(1000);
+        Serial.println("Waiting GSM...");
+    }
+    
+    // while(!Serial) delay(1000); // attente affichage écran
+    Serial.println("GSM OK!!");
+    
+    digitalWrite(13, HIGH);             // LED on
+    delay(1000);
+    digitalWrite(13, LOW);              // LED off
+    delay(100);
+    digitalWrite(13, HIGH);             // LED on
+    delay(1000);
+    digitalWrite(13, LOW);              // LED off
+    delay(100);
+    
+  delay(3000);
+}
+
+void loop() {
+  Serial.println("***************"); 
+  //TCPsock(); // pilotage de la LED via le site internet MCS
+  //Serial.println("LGPS loop"); 
+  LGPS.getData(&info);
+  Serial.println("Trame GPGGA :"); 
+  //Serial.println((char*)info.GPGGA); //affichage trame brute GPGGA
+  Serial.println("Donnees recoltees :"); 
+  parseGPGGA((const char*)info.GPGGA); // traitement de la trame GPGGA
+  connectGSM(); //traitement des SMS
+  delay(2000);
+}
